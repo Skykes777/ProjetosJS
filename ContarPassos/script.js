a0f55e94fb821aafd43c539d69ca708e65ad8e5a
Skykes777 <alejandro777kgb@gmail.com>
2022-10-17 13:24:26 -0300

    Conta_passos

---
+++ ContarPassos/script.js
@@ -0,0 +1,30 @@
+function contar() {
+    i = document.querySelector('input#Ti')
+    f = document.querySelector('input#Tf')
+    p = document.querySelector('input#Tp')
+    res = document.querySelector('div#resultado')
+    if (p.value.length == 0 || p.value <= 0 ) {
+        alert('[ERRO] O valor de passos não pode ser menor ou igual a zero!')
+    } else {
+        // Valores abaixo sendo alterados para number
+        i = Number(i.value)
+        f = Number(f.value)
+        p = Number(p.value)
+
+        if (i < f) {  // Condição caso o início sejá menor que o final
+            res.innerHTML = 'Contando...<br>'
+            for (i; i<=f; i+=p) {
+                res.innerHTML += `${i} 👉🏻 `
+            }
+            res.innerHTML += '🏁 FIM!!'
+        } else if (i > f) {  // Condição caso o início sejá maior que o final
+            res.innerHTML = 'Contando...<br>'
+            for (i; i>=f; i-=p) {
+                res.innerHTML += `${i} 👉🏻 `
+            }
+            res.innerHTML += '🏁 FIM!!'
+        } else {
+            alert('[ERRO]! O valor do início não pode ser igual ao final e vice-versa!')
+        }
+    }
+}
